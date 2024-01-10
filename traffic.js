117c649a96b0b37184f850fe66efc607891f7091
ByteWarlord <156203171+ByteWarlord@users.noreply.github.com>
2024-01-11 02:08:21 +0800

    Update traffic.js

--- traffic.js
+++ traffic.js
@@ -170,5 +170,5 @@ function formatTime(time) {
   let day = dateObj.getDate();
 	let day1 = day<9?"0"+day:day;
   //return year + " 年 " + month1+ " 月 " + day1+" 日";
-	return new Intl.DateTimeFormat('en-US', { dateStyle: 'long' }).format(dateObj)+"";
+	return new Date(time) +"";
 }
