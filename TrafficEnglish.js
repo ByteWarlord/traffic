9f5cd6395cd90f7dd79133314c0f1647be7b23fd
ByteWarlord <156203171+ByteWarlord@users.noreply.github.com>
2024-01-13 00:41:26 +0800

    Update TrafficEnglish.js

--- TrafficEnglish.js
+++ TrafficEnglish.js
@@ -163,5 +163,6 @@ function formatTime(time) {
   // 检查时间戳是否为秒单位，如果是，则转换为毫秒
   if (time < 1000000000000) time *= 1000;
   let dateObj = new Date(time);
-	return new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(dateObj);
+	//return new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(dateObj);
+	return date.toLocaleString('zh-CN', {timeZone: 'Asia/Shanghai',dateStyle:'full',timeStyle:'medium'});
 }
