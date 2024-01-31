3176d14b12ff807c944c5f4f4a3ebafd08264da3
ByteWarlord <156203171+ByteWarlord@users.noreply.github.com>
2024-01-31 11:28:06 +0800

    Update Daily.js

--- Daily.js
+++ Daily.js
@@ -38,7 +38,7 @@
     // 到期时间（日期）显示
     if (expireDaysLeft) {
 			let expireDays = 
-      content.push(`${formatTime(args.expire || info.expire)}｜${expireDaysLeft}`);
+      content.push(`${formatTime(args.expire || info.expire)} ${expireDaysLeft}`);
     }
   }
 
