cf59d0ccead2ab2afd69642819926faac14d7749
ByteWarlord <156203171+ByteWarlord@users.noreply.github.com>
2024-08-13 15:51:33 +0800

    Update Daily.js

--- Daily.js
+++ Daily.js
@@ -26,8 +26,11 @@
   let content = [
 		//`流量 ${bytesToSize(used)}｜${bytesToSize(total)}`];
 // `流量 ${bytesToSize(used)}｜${bytesToSize(total)}`];
-	  `上传流量：${bytesToSize(upload)}
-   下载流量：${bytesToSize(download)}`];
+	  `
+   上传流量：${bytesToSize(upload)}
+   下载流量：${bytesToSize(download)}
+   剩余流量：${bytesToSize(unused)}
+   流量套餐：${bytesToSize(total)}`];
 
   // 判断是否为不限时套餐
   if (!resetDayLeft && !expireDaysLeft) {
@@ -35,7 +38,13 @@
     content.push(`⏰ 不限时套餐`);
   } else {
     if (resetDayLeft && expireDaysLeft) {
-    content.push(`还有${resetDayLeft}天重置，${expireDaysLeft}天到期`);
+    // content.push(`还有${resetDayLeft}天重置，${expireDaysLeft}天到期`);
+	        content.push(
+			`
+   重置天数：${resetDayLeft}
+   到期天数：${expireDaysLeft}
+   `
+		);
     } else if (resetDayLeft) {
     //content.push(`PER    ${proportion(used,total)}  🌸 Reset ${resetDayLeft} `+afterday);
       content.push(`提醒：套餐将在${resetDayLeft}天后重置`);
@@ -47,7 +56,7 @@
     // 到期时间（日期）显示
     if (expireDaysLeft) {
 			let expireDays = 
-      content.push(`${formatTime(args.expire || info.expire)}`);
+      content.push(`到期时间：${formatTime(args.expire || info.expire)}`);
     }
   }
 
